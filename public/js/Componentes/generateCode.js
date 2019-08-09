1504a142820a264f82ea3a5f0ed9b373983afbeb
Carlos <carloshernandez.chsegunda@gmail.com>
2019-08-08 20:07:03 -0600

    Se termino de programar el select

--- public/js/Componentes/generateCode.js
+++ public/js/Componentes/generateCode.js
@@ -13,20 +13,73 @@ function generarCodigo(){
     console.log(code);
 }
 
-
+//--------------------------------------------------------------------- SELECT----------------------------------------------------------
 Blockly.JavaScript['select'] = function(block) {
-    console.log(block);
+    //console.log(block);
     var code = "SELECT ";
-    if(block.childBlocks_.length > 0) code += getCampos(block.childBlocks_[0]) + "\n";
+
+    //------------------------------------------------- CAMPOS EN SELECT --------------------------------------------------------------
+    if(block.childBlocks_[0].getFieldValue('campo')) code += getCampos(block.childBlocks_[0]);
+    else if(block.childBlocks_[0].getFieldValue('Tabla')){
+        let tabla = block.childBlocks_[0].getFieldValue('Tabla');
+        let campo = block.childBlocks_[0].getFieldValue('Campos');
+        code+= tabla + "." + campo;
+    }else if(block.childBlocks_[0].getFieldValue('valor')) code +=  block.childBlocks_[0].getFieldValue('valor');
+
+    code += "\n";
+    //-------------------------------------------------- CAMPOS DE FROM--------------------------------------------------------------------
+
     code+= "FROM ";
-    if(block.childBlocks_.length > 1) code += getTablas(block.childBlocks_[1]) + "\n";
+    if(block.childBlocks_[1].getFieldValue('campo')) code += getCampos(block.childBlocks_[1]) + "\n";
+    else if(block.childBlocks_[1].getFieldValue('valor')) code +=  block.childBlocks_[1].getFieldValue('valor');
+
+    code += "\n";
+
+    //-------------------------------------------------- WHERE -----------------------------------------------------------------------------
+    if(block.childBlocks_.length[2]) if(block.childBlocks_.length[2].getFieldValue('WHERE')) code += block.childBlocks_.length[2].getFieldValue('WHERE');
+
     return code;
 };
-
+//---------------------------------------------------------------------- CAMPOS DE TEXTO---------------------------------------------------
 Blockly.JavaScript['campo'] = function(block) {
     var text = block.getFieldValue('campo');
     return [text];
 };
+
+//----------------------------------------------------------------------- WHERE ---------------------------------------------------------------
+Blockly.JavaScript['where'] = function(block){
+    let code = "WHERE ";
+    if(block.childBlocks_[0]) code += resolverOperacion(block.childBlocks_[0])
+    code += "\n";
+    return code;
+}
+
+//--------------------------------------------------------------------------- resolver las operaciones ---------------------------------------------------
+function resolverOperacion(operacion){
+    if(operacion.getFieldValue("valor")) return operacion.getFieldValue("valor");
+    else if (operacion.getFieldValue("OPERA")){
+        let opera1 = null;
+        let opera2 = null;
+        let operando = operacion.getFieldValue("OPERA");
+        if(operacion.childBlocks_[0]) opera1 = resolverOperacion(operacion.childBlocks_[0]);
+        if(operacion.childBlocks_[1]) opera2 = resolverOperacion(operacion.childBlocks_[1]);
+        return opera1 + " " + operando + " " + opera2;
+    }else if(operacion.getFieldValue("ARITMETICA")){
+        let opera1 = null;
+        let opera2 = null;
+        let operando = operacion.getFieldValue("ARITMETICA");
+        if(operacion.childBlocks_[0]) opera1 = resolverOperacion(operacion.childBlocks_[0]);
+        if(operacion.childBlocks_[1]) opera2 = resolverOperacion(operacion.childBlocks_[1]);
+        return opera1 + " " + operando + " " + opera2;
+    }
+    return "";
+}
+
+//-------------------------------------------------------------------- condiciones booleanas ---------------------------------------------------
+Blockly.JavaScript['condicion'] = function(block){
+    console.log(block);
+    return "condicion";
+}
 
 //------------------------------------------------ concatena los campos de un select ------------------------------------------------------------
 function getCampos(hijo){
--- public/js/Componentes/sql.js
+++ public/js/Componentes/sql.js
@@ -1,16 +1,16 @@
 //--------------------------------- bases-----------------------------------------------------------------------
-var campos1 = [["*","*"],["titulo1","titulo1"]];
-var campos2 = [["*","*"],["titulo2","titulo2"]];
+var campos1 = [["*", "*"], ["titulo1", "titulo1"]];
+var campos2 = [["*", "*"], ["titulo2", "titulo2"]];
 
 var tablas = [
-    {
-        "nombre" : "Tabla1",
-        "campos" : campos1
-    },
-    {
-        "nombre" : "Tabla2",
-        "campos" : campos2
-    }
+  {
+    nombre: "Tabla1",
+    campos: campos1
+  },
+  {
+    nombre: "Tabla2",
+    campos: campos2
+  }
 ];
 
 /*---------------------------------------------------------------------------------------------------------------
@@ -19,78 +19,159 @@ var tablas = [
 
 //--------------------------------------- SELECT ----------------------------------------------------------------
 
-Blockly.Blocks['select'] = {
-    init: function() {
-        this.appendValueInput("SELECT")
-            .setCheck("String")
-            .appendField("SELECT");
-        this.appendValueInput("FROM")
-            .setCheck(null)
-            .appendField("FROM");
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setOutput(false, null);   
-     this.setTooltip("");
-     this.setHelpUrl("");
-      }
-    };
+Blockly.Blocks["select"] = {
+  init: function() {
+    this.appendValueInput("SELECT")
+      .setCheck("String")
+      .appendField("SELECT");
+    this.appendValueInput("FROM")
+      .setCheck(null)
+      .appendField("FROM");
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setOutput(false, null);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
 //------------------------------------------ TABLAS --------------------------------------------------------------
-Blockly.Blocks['Tablas'] = {
-    init: function(){
-        var defaultlist = [ ["Tabla1","Tabla1" ], ["Tabla2","Tabla2" ] ];
-        var defaultlist2 = [ ["*","*" ] ];
-        if(tablas.length > 0) defaultlist2 = tablas[0].campos
-        
-        this.setColour(122);
-        this.appendDummyInput('dropDownField')
-        .appendField(new Blockly.FieldDropdown(defaultlist), 'Tabla');
-        
-        this.appendDummyInput('dropDownField2')   
-        .appendField(new Blockly.FieldDropdown(defaultlist2), 'Campos');       
-        
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-     this.setTooltip("");
-     this.setHelpUrl("");
-    },
-    onchange : function(e){
-        //---------------- que si cambio un campo y no fue un click
-        if(e.element === "field" ){
-            //------------------- lo que cambio fue una tabla y no un campo
-            if(e.name === "Tabla"){
-                var dropdown = this.getInput('dropDownField').fieldRow[0];
-                var segunda = buscarListas(dropdown.value_);
-                if(segunda){
-                    try{
-                        this.removeInput('dropDownField2');
-                        this.appendDummyInput('dropDownField2').appendField(
-                            new Blockly.FieldDropdown(segunda), 'String');
-                    }catch (ee) { } 
-                }
-            }
+Blockly.Blocks["Tablas"] = {
+  init: function() {
+    var defaultlist = [["Tabla1", "Tabla1"], ["Tabla2", "Tabla2"]];
+    var defaultlist2 = [["*", "*"]];
+    if (tablas.length > 0) defaultlist2 = tablas[0].campos;
+
+    this.setColour(122);
+    this.appendDummyInput("dropDownField").appendField(
+      new Blockly.FieldDropdown(defaultlist),
+      "Tabla"
+    );
+
+    this.appendDummyInput("dropDownField2").appendField(
+      new Blockly.FieldDropdown(defaultlist2),
+      "Campos"
+    );
+
+    this.setOutput(true, null);
+    this.setColour(120);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  },
+  onchange: function(e) {
+    //---------------- que si cambio un campo y no fue un click
+    if (e.element === "field") {
+      //------------------- lo que cambio fue una tabla y no un campo
+      if (e.name === "Tabla") {
+        var dropdown = this.getInput("dropDownField").fieldRow[0];
+        var segunda = buscarListas(dropdown.value_);
+        if (segunda) {
+          try {
+            this.removeInput("dropDownField2");
+            this.appendDummyInput("dropDownField2").appendField(
+              new Blockly.FieldDropdown(segunda),
+              "String"
+            );
+          } catch (ee) {}
         }
-    } 
-}
+      }
+    }
+  }
+};
 
+//------------------------------------------------------- campos de texto-------------------------------------------------
+Blockly.Blocks["campo"] = {
+  init: function() {
+    this.appendValueInput("VALUE")
+      .setCheck("String")
+      .appendField(new Blockly.FieldTextInput("*"), "campo");
+    this.setOutput(true, "String");
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
 
+//-------------------------------------------------- CONDICIONES ----------------------------------------------------------
+Blockly.Blocks["condicion"] = {
+  init: function() {
+    this.appendValueInput("Op1")
+      .setCheck(null)
+      .setAlign(Blockly.ALIGN_CENTRE);
+    this.appendValueInput("op2")
+      .setCheck(null)
+      .appendField(
+        new Blockly.FieldDropdown([
+          [">", ">"],
+          ["<", "<"],
+          ["==", "=="],
+          ["<=", "<="],
+          ["!=", "!="],
+          [">=", ">="]
+        ]),
+        "OPERA"
+      );
+    this.setOutput(true, null);
+    this.setColour(200);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
 
-Blockly.Blocks['campo'] = {
+//-------------------------------------------------- CONDICIONES ----------------------------------------------------------
+Blockly.Blocks["operaciones"] = {
     init: function() {
-      this.appendValueInput("VALUE")
-          .setCheck("String")
-          .appendField(new Blockly.FieldTextInput("*"), "campo");
-      this.setOutput(true, "String");
-      this.setColour(150);
-   this.setTooltip("");
-   this.setHelpUrl("");
+      this.appendValueInput("Op1")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_CENTRE);
+      this.appendValueInput("op2")
+        .setCheck(null)
+        .appendField(
+          new Blockly.FieldDropdown([
+            ["+", "+"],
+            ["-", "-"],
+            ["*", "*"],
+            ["/", "/"],
+          ]),
+          "ARITMETICA"
+        );
+      this.setOutput(true, null);
+      this.setColour(200);
+      this.setTooltip("");
+      this.setHelpUrl("");
     }
   };
 
+//--------------------------------------------------- WHERE --------------------------------------------------------------
+Blockly.Blocks["where"] = {
+  init: function() {
+    this.appendValueInput("WHERE")
+      .setCheck(null)
+      .appendField("WHERE");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
 
-function buscarListas(nombre){
-    for(let i = 0; i < tablas.length; i++){
-        if(tablas[i].nombre === nombre) return tablas[i].campos;
-    }
-    return null;
+//-------------------------------------------------- valor puntual -----------------------------------------------------
+Blockly.Blocks["valor"] = {
+  init: function() {
+    this.appendDummyInput().appendField(
+      new Blockly.FieldTextInput("valor"),
+      "valor"
+    );
+    this.setOutput(true, null);
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+function buscarListas(nombre) {
+  for (let i = 0; i < tablas.length; i++) {
+    if (tablas[i].nombre === nombre) return tablas[i].campos;
+  }
+  return null;
 }
